1fb85a546548a056f3896890384d650d773ace08
KoblizekXD <janprokupek@centrum.cz>
2024-08-22 18:36:34 +0200

    Initial Commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import { auth } from "./auth/auth";
+
+const app = new Elysia()
+  .group('/api/auth', app => app.use(auth))
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
