813845a807e57ed70445b86f249c30b2c9769d1f
KoblizekXD <janprokupek@centrum.cz>
2024-08-24 19:55:15 +0200

    Add authorization handling

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
-import { Elysia } from "elysia";
-import { PrismaClient } from "@prisma/client";
+import Elysia, { error } from "elysia";
+import { Permission, PrismaClient } from "@prisma/client";
+
+import { pino } from "pino";
+import { jwt } from '@elysiajs/jwt'
 
 import { auth } from "./auth/auth";
+import { AppModule } from "./util/app";
 
 export const prisma = new PrismaClient();
+export const logger = pino();
+export const jwtConfig = jwt({
+  name: 'jwt',
+  secret: process.env.JWT_SECRET ?? (() => { throw new Error('JWT Secret was not found.') })(),
+  exp: `${parseInt(process.env.TOKEN_EXP!) / 60 / 60 / 24}d`
+})
 
 const app = new Elysia()
+  .use(AppModule)
   .group('/api/auth', app => app.use(auth))
   .listen(3000);
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+  
+logger.info(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
---
+++ src/util/app.ts
@@ -0,0 +1,64 @@
+import Elysia, { error as logErr } from "elysia";
+import { jwt, JWTPayloadSpec } from "@elysiajs/jwt";
+import { Permission } from "@prisma/client";
+import { prisma } from "..";
+
+type jwtManager = {
+  readonly sign: (morePayload: Record<string, string | number> & JWTPayloadSpec) => Promise<string>;
+  readonly verify: (jwt?: string) => Promise<(Record<string, string | number> & JWTPayloadSpec) | false>;
+}
+
+export class AuthError extends Error {
+  constructor(public required?: Permission[]) {
+    super('Unauthorized');
+  }
+}
+
+export class AuthManager {
+  constructor(public authorization: string | undefined, public jwtManager: jwtManager) { }
+
+  /**
+   * Checks if the user is authenticated.
+   * @example
+   * if (await authManager.authenticated()) {
+   *  // User is authenticated.
+   * }
+   * @returns {boolean} Whether the user is authenticated.
+   */
+  public async authenticated(): Promise<boolean> {
+    if ((this.authorization !== undefined && await this.jwtManager.verify(this.authorization) !== false))
+      return true;
+    else throw new AuthError();
+  }
+
+  /**
+   * Checks if the user is authorized && authenticated with permissions.
+   * 
+   * @param permissions permissions required to access the route.
+   * @returns {boolean} Whether the user is authorized.
+   */
+  public async authorized(permissions: Permission[] = []): Promise<boolean> {
+    const token = await this.jwtManager.verify(this.authorization);
+    if (!token) throw new AuthError(permissions);
+
+    const user = await prisma.user.findUnique({ where: { email: token.sub } });
+    if (!user || user.permissions.every(item => permissions.includes(item))) throw new AuthError(permissions);
+
+    return true;
+  }
+}
+
+export const AppModule = new Elysia({ seed: 'auth_app_module' })
+  .use(jwt({
+    name: 'jwt',
+    secret: process.env.JWT_SECRET ?? (() => { throw new Error('JWT Secret was not found.') })(),
+    exp: `${parseInt(process.env.TOKEN_EXP!) / 60 / 60 / 24}d`
+  }))
+  .error({ AuthError })
+  .onError({ as: 'global' }, ({ code, error }) => {
+    if (code === 'AuthError')
+      return logErr('Unauthorized', { message: 'You are not authorized to access this route.', required: error.required });
+  })
+  .derive({ as: 'global' }, ({ headers: { authorization }, jwt }) => ({
+    authManager: new AuthManager(authorization, jwt)
+  }));
