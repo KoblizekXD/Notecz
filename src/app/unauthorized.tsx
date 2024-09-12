8405fe8f7d647d4a7f9b63933a8eb260e8b5d770
KoblizekXD <janprokupek@centrum.cz>
2024-09-12 14:48:35 +0200

    Add default page & middleware

---
+++ src/app/unauthorized.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+
+export default function NotFound() {
+  return (
+    <main
+      className={
+        'flex gap-y-16 justify-center items-center flex-col min-h-screen w-full'
+      }
+    >
+      <div className={'flex items-baseline -translate-x-[17rem]'}>
+        <h1 className="font-semibold text-7xl">Jejda!</h1>
+        <h4 className="text-2xl font-mono text-gray-500">(401)</h4>
+      </div>
+      <h2 className="font-bold translate-x-[9rem] text-4xl">
+        K tomuto obsahu aktuálně nemáte přístup
+      </h2>
+    </main>
+  );
+}
